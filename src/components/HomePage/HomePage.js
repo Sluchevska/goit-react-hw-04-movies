ab3cc1d06a9ec4720ec793bdfd0745438d79d2f3
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-17 21:24:18 +0100

    add useroute

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,25 +1,29 @@
-import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-import * as MovieApi from '../../services/movie-api'
-
+import { useEffect, useState } from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import * as MovieApi from '../../services/movie-api';
 
 export default function HomePage() {
-    const [movies, setMovies] = useState(null)
-    useEffect(() => {
-        MovieApi.fetchPopularMovie().then(data => { setMovies(data.results) })
-},[])
+  const { url } = useRouteMatch();
 
-    return (
-        <>
-            <h1>Popular movies</h1>
-            {movies && movies.map(movie =>
-                <li key={movie.id}>
-                    <Link to={`/movies/${movie.original_title}`}>
-                    {movie.original_title}
-                    </Link>
-                    </li>
+  const [movies, setMovies] = useState(null);
 
-            )}
-            </>
-    )
+  useEffect(() => {
+    MovieApi.fetchPopularMovie().then(data => {
+      setMovies(data.results);
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>Popular movies</h1>
+      {movies &&
+        movies.map(movie => (
+          <li key={movie.id}>
+            <Link to={`${url}${movie.original_title}`}>
+              {movie.original_title}
+            </Link>
+          </li>
+        ))}
+    </>
+  );
 }
