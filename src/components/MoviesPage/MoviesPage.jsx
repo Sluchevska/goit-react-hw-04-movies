f37b06fdceed4d7e88ae88a5a2e826dcdb1cdff2
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-19 19:45:49 +0100

    update

--- src/components/MoviesPage/MoviesPage.jsx
+++ src/components/MoviesPage/MoviesPage.jsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from 'react';
 import SearchBar from '../SearchBar/SearchBar';
 import * as MovieApi from '../../services/movie-api';
@@ -6,72 +5,67 @@ import { NavLink, useRouteMatch } from 'react-router-dom';
 import LoadMoreBtnClick from '../LoadMoreBtn/LoadMoreBtn';
 
 export default function MoviesPage() {
-   const { url } = useRouteMatch();
- 
+  const { url } = useRouteMatch();
+
   const [movies, setMovies] = useState([]);
   const [searchName, setSearchName] = useState('');
-  const [page, setPage]=useState(1)
- 
+  const [page, setPage] = useState(1);
+
   useEffect(() => {
-    if (!searchName) return
-    
+    if (!searchName) return;
+
     MovieApi.fetchMovieByName(searchName, page).then(data => {
-      setMovies(data.results)
+      setMovies(data.results);
       // setMovies(prevMovies=>[...prevMovies, ...data.results]);
-    //   page > 1 &&
-    //       window.scrollTo({
-    //         top: document.documentElement.scrollHeight,
-    //         behavior: 'smooth',
-    //       });
+      //   page > 1 &&
+      //       window.scrollTo({
+      //         top: document.documentElement.scrollHeight,
+      //         behavior: 'smooth',
+      //       });
     });
   }, [page, searchName]);
 
   console.log(movies);
 
- 
   const handleSubmit = searchName => {
-    console.log(searchName)
-    setSearchName(searchName)
-  }
+    console.log(searchName);
+    setSearchName(searchName);
+  };
 
   const loadMoreBtnClick = () => {
     setPage(prevPage => prevPage + 1);
   };
-    
+
   const showButton = movies.length > 20;
-  console.log(showButton)
-   
-  
+
   return (
     <>
-      <SearchBar  onSearch={handleSubmit} />
+      <SearchBar onSearch={handleSubmit} />
       <h1>Its movies Page</h1>
-      {movies &&(
+      {movies && (
         <ul>
-        {movies.map(movie => (
-          <li key={movie.id}>
-            <NavLink to={`${url}/${movie.id}`}>{movie.original_title}</NavLink>
-          </li>
-        ))}
-        </ul>)
-      }
+          {movies.map(movie => (
+            <li key={movie.id}>
+              <NavLink to={`${url}/${movie.id}`}>
+                {movie.original_title}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      )}
       {showButton && <LoadMoreBtnClick onClick={loadMoreBtnClick} />}
-   
     </>
   );
 }
 
-
-
-
 // export default function MoviesPage() {
- 
+
 //   const [movies, setMovies] = useState(null);
 
 //   const SearchMovie = (query) => {
 //   useEffect(() => {
 //     // if (!searchName) return
-    
+
 //     MovieApi.fetchMovieByName(query).then(data => {
 //       setMovies(data.results);
 //     });
@@ -79,10 +73,9 @@ export default function MoviesPage() {
 
 //   console.log(movies);
 //   console.log(query)
-    
+
 // }
-   
-  
+
 //   return (
 //     <>
 //       <SearchBar  onSubmit={SearchMovie} />
