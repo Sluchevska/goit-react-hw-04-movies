36b33bcde7ee30fe49be171aa172270ab19b3d4f
Aleksanrda Sluchevska <alaska3a@gmail.com>
2021-09-19 17:19:14 +0100

    update

--- src/components/MoviesPage/MoviesPage.jsx
+++ src/components/MoviesPage/MoviesPage.jsx
@@ -1,16 +1,65 @@
-import { useState } from 'react';
+
+import { useEffect, useState } from 'react';
 import SearchBar from '../SearchBar/SearchBar';
+import * as MovieApi from '../../services/movie-api';
 
 export default function MoviesPage() {
-    const [searchName, setSearchName] = useState([]);
+ 
+  const [movies, setMovies] = useState(null);
+ const [searchName, setSearchName] = useState('');
+ 
+  useEffect(() => {
+    if (!searchName) return
     
+    MovieApi.fetchMovieByName(searchName).then(data => {
+      setMovies(data.results);
+    });
+  }, [searchName]);
+
+  console.log(movies);
+  console.log(searchName)
+ 
   const handleSubmit = searchName => {
-    setSearchName(searchName);
-  };
+    setSearchName(searchName)
+  }
+    
+
+   
+  
   return (
     <>
-      <SearchBar onSearch={handleSubmit} />
+      <SearchBar  onSearch={handleSubmit} />
       <h1>Its movies Page</h1>
     </>
   );
 }
+
+
+
+
+// export default function MoviesPage() {
+ 
+//   const [movies, setMovies] = useState(null);
+
+//   const SearchMovie = (query) => {
+//   useEffect(() => {
+//     // if (!searchName) return
+    
+//     MovieApi.fetchMovieByName(query).then(data => {
+//       setMovies(data.results);
+//     });
+//   }, [query]);
+
+//   console.log(movies);
+//   console.log(query)
+    
+// }
+   
+  
+//   return (
+//     <>
+//       <SearchBar  onSubmit={SearchMovie} />
+//       <h1>Its movies Page</h1>
+//     </>
+//   );
+// }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,64 +1,68 @@
-import { useEffect, useState } from 'react';
-import { useRouteMatch } from 'react-router';
-// import { Link } from 'react-router-dom';
-import * as MovieApi from '../../services/movie-api';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
 
+export default function SearchBar({ onSearch }) {
+//   const [searchName, setSearchName] = useState('');
 
-
-
-// export default function Movies(searchName) {
-//     const { url } = useRouteMatch();
-//   const [movies, setMovies] = useState(null);
-
-//   useEffect(() => {
-//     MovieApi.fetchMovieByName(searchName).then(data => {
-//       setMovies(data);
-//     });
-//   }, [searchName]);
-//   console.log(movies);
-//   return (
-//     <>
-//       <form>
-//         <button type='button'>
-//           <span>Search</span>
-//         </button>
-//               <input
-//                   name="searchName"
-//           type="text"
-//           autoComplete="off"
-//           autoFocus
-//           placeholder="Enter movie name"
-//               />
-        
-//       </form>
-//     </>
-//   );
-// }
-
-export default function SearchBar({ onSearch }){
     const handleSearch = e => {
-        console.log(e.target.elements.searchName.value)
         e.preventDefault();
-        onSearch(e.target.elements.searchName.value.toLowerCase())
-        
-    }
-    return (
-        <>
-       <form onSubmit={handleSearch}>
-         
-               <input
-                  name="searchName"
+        // console.log(e.target.elements.searchName.value)
+    onSearch(e.target.elements.searchName.value.toLowerCase());
+  };
+ 
+  return (
+    <>
+      <form onSubmit={handleSearch}>
+        <input
+          name="searchName"
           type="text"
           autoComplete="off"
           autoFocus
           placeholder="Enter movie name"
-                />
-                <button type='button'>
-           <span>Search</span>
-         </button>
-        
+        //   onChange={handleSearch}
+        />
+        <button type="submit">
+          <span>Search</span>
+        </button>
       </form>
     </>
-    )
-
+  );
 }
+
+
+
+
+
+// export default function SearchBar({ onSubmit }) {
+//   const [searchName, setSearchName] = useState('');
+
+//   const handleSearch = e => {
+//     setSearchName(e.target.value.toLowerCase());
+//   };
+//   const handleSubmit = e => {
+//       e.preventDefault();
+//       if (searchName.trim() === '') {
+//       toast.info('Please enter your query!');
+//       return;
+//     }
+//     onSubmit(searchName);
+//     setSearchName('');
+//   };
+//   return (
+//     <>
+//       <form onSubmit={handleSubmit}>
+//         <input
+//           name="searchName"
+//           type="text"
+//           autoComplete="off"
+//           autoFocus
+//           placeholder="Enter movie name"
+//           onChange={handleSearch}
+//         />
+//         <button type="submit">
+//           <span>Search</span>
+//         </button>
+//       </form>
+//     </>
+//   );
+// }
